37c6e85dd097b53c8023ddc6d082d89b3940b28b
tugalsan karabacak <tugalsan@gmail.com>
2023-04-11 17:45:13 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module com.tugalsan.api.console {
-    requires com.tugalsan.api.executable;
     requires com.tugalsan.api.charset;
-    requires com.tugalsan.api.stream;
-    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.executable;
     exports com.tugalsan.api.console.server;
 }
